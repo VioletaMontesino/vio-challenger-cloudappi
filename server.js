37740f6d2e6c96a8cabc5bcd72cc0b2c56fe179c
VioletaMontesino <viomont76@gmail.com>
2021-11-08 22:47:12 +0100

    names changed

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/crud-angular-mongo-express'));
+app.use(express.static(__dirname + '/dist/front'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/crud-angular-mongo-express/index.html'));});
+    '/dist/front/src/index.html'));});
 app.listen(process.env.PORT || 8080);
